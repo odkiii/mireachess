873cc488fb70dd73e12d73539e69e9aca9ea5f3e
odkiii <ivan.balybredin@gmail.com>
2025-02-26 15:16:39 +0300

    1 commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MireaChess from './components/ChessGame.vue'
+</script>
+
+<template>
+
+  <MireaChess msg="MireaChess" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ChessGame.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { TheChessboard } from "vue3-chessboard";
+import "vue3-chessboard/style.css";
+</script>
+
+<template title>
+  <div>
+    <h1>MireaChess</h1>
+    <div id="board">
+      <TheChessboard/>
+    </div>
+    <div id="status">ходят белые</div>
+    <div id="moves">e1m1</div>
+    <div className="menu">
+      <button>Ход назад</button>
+      <button>Ход вперед</button>
+      <button>Игра заново</button>
+      <select>
+        <option value="1">Слабый бот</option>
+        <option value="2">Средний бот</option>
+        <option value="3">Сильный бот</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<style>
+#board {
+  margin: 20px ;
+  border: 2px solid black;
+  width: auto;
+  height: auto;
+}
+
+.menu {
+  display: grid;
+  grid-template-columns: 20% 20% 20% 40%;
+  margin: auto;
+  width: 400px;
+  height: auto;
+  border: 2px solid black;
+}
+
+.menu button {
+  margin: 10px;
+}
+
+.menu select {
+  margin: 10px;
+}
+</style>
